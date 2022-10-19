c7d200a4166129afb48762ebf06bfd09bccca05c
Alcareh <69974175+Alcareh@users.noreply.github.com>
2022-10-19 16:38:24 -0500

    Ojito show/hide password

--- calibracion HoloOptypp/Assets/Script/IntroScene/IntroManager.cs
+++ calibracion HoloOptypp/Assets/Script/IntroScene/IntroManager.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class IntroManager : MonoBehaviour
 {
     [Header("User UX")] 
-    [SerializeField] private GameObject showPassword;
+    [SerializeField] private TMPro.TMP_InputField passwordField;
+
+    public void ShowHidePassword()
+    {
+        if (passwordField.contentType == TMP_InputField.ContentType.Password)
+        {
+            passwordField.contentType = TMP_InputField.ContentType.Standard;
+        }
+        else
+        {
+            passwordField.contentType = TMP_InputField.ContentType.Password;
+        }
+        passwordField.ForceLabelUpdate();
+    }
 }
