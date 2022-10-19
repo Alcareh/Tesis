6bdde0f97ed7d406ed9ee8bce92551a8a51e260d
Alcareh <69974175+Alcareh@users.noreply.github.com>
2022-10-19 17:42:20 -0500

    Ojitos working
    Además de servir, se acabaron 3 vistas, falta es comunicarlas y ponerle las interacciones (además de sonidos)

--- calibracion HoloOptypp/Assets/Script/IntroScene/IntroManager.cs
+++ calibracion HoloOptypp/Assets/Script/IntroScene/IntroManager.cs
@@ -2,22 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroManager : MonoBehaviour
 {
-    [Header("User UX")] 
-    [SerializeField] private TMPro.TMP_InputField passwordField;
-
-    public void ShowHidePassword()
-    {
-        if (passwordField.contentType == TMP_InputField.ContentType.Password)
-        {
-            passwordField.contentType = TMP_InputField.ContentType.Standard;
-        }
-        else
-        {
-            passwordField.contentType = TMP_InputField.ContentType.Password;
-        }
-        passwordField.ForceLabelUpdate();
-    }
+    
 }
