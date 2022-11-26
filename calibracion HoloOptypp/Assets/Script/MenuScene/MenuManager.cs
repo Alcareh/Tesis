7604e2a56e791af20c68c8cb446e7cda617a5a97
Alcareh <69974175+Alcareh@users.noreply.github.com>
2022-11-25 23:16:03 -0500

    Selección correcta de personaje, hace falta cambiar el fondo gris por un white square with circle border

--- calibracion HoloOptypp/Assets/Script/MenuScene/MenuManager.cs
+++ calibracion HoloOptypp/Assets/Script/MenuScene/MenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using  TMPro;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -12,6 +13,16 @@ public class MenuManager : MonoBehaviour
     [SerializeField] public TMP_Text logrosText;
     [SerializeField] public TMP_Text inventarioText;
     [SerializeField] public TMP_Text retosText;
+
+    [Header("Primera vez avatar")] 
+    [SerializeField] private GameObject welcomeOnce;
+    [SerializeField] private GameObject bgContainer;
+    [SerializeField] private GameObject avatarContainer;
+
+    [Header("Profile Info")] 
+    [SerializeField] private GameObject fakeProfile;
+    [SerializeField] private GameObject profileBG;
+    [SerializeField] private GameObject profileAvatar;
     
     public void Inicio()//Cambia a menú inicio
     {
@@ -31,5 +42,21 @@ public class MenuManager : MonoBehaviour
     public void Retos()//Cambia a menú retos
     {
         headerBar.GetComponent<HeaderBar>().MoverBarra(retosText.transform.position, -10f);
+    }
+    
+    public void AceptarAvatarInicio(){
+        if (bgContainer.GetComponent<SelectedAvatar>().selectedAvatar!=null&&avatarContainer.GetComponent<SelectedAvatar>().selectedAvatar!=null)
+        {
+            profileBG.GetComponent<Image>().sprite = bgContainer.GetComponent<SelectedAvatar>().selectedAvatar;
+            profileAvatar.GetComponent<Image>().sprite = avatarContainer.GetComponent<SelectedAvatar>().selectedAvatar;
+            //Por acá toca mandarle lo datos de selección del usuario al backend.
+            if (fakeProfile.activeSelf)
+            {
+                fakeProfile.SetActive(false);
+            }
+            welcomeOnce.SetActive(false);
+        }
+        //Debería hacer un toast para decir que seleccione al menos uno xdxd
+        Debug.Log("Escoja algo bruto");
     }
 }
